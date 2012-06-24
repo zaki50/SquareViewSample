f2864c9445601fd94ef5ecbd800683e4abd68178
Makoto Yamazaki <zaki@uphyca.com>
2012-06-24 21:39:13 +0900

    正方形のView のサンプル作った

---
+++ src/org/zakky/square/SquareView.java
@@ -0,0 +1,34 @@
+
+package org.zakky.square;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SquareView extends View {
+
+    public SquareView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SquareView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        int smaller = Math.min(width, height);
+
+        setMeasuredDimension(smaller, smaller);
+    }
+
+}
---
+++ src/org/zakky/square/SquareViewSampleActivity.java
@@ -0,0 +1,13 @@
+package org.zakky.square;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SquareViewSampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
